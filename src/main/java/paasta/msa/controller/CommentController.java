36bb0010c2f4163e797801fede78bcbb1a4e823c
Im-wali <dlawowo01@gmail.com>
2021-08-24 15:26:18 +0900

    기능추가
    기능추가

--- src/main/java/paasta/msa/controller/CommentController.java
+++ src/main/java/paasta/msa/controller/CommentController.java
@@ -1,6 +1,5 @@
 package paasta.msa.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
